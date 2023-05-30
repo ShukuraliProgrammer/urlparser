c75b2ea2e0765eb0c894ccb400718e54f0f24a6e
ShukuraliProgrammer <shukuralirezamanov@gmail.com>
2023-05-30 11:34:44 +0500

    del: not need lines

--- urlpapp/models.py
+++ urlpapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
+
 class UrlParser(models.Model):
     url = models.CharField(max_length=100)
     text = models.CharField(max_length=100)
